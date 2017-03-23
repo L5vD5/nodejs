88d70976870a999505c663e3516f4cb17c6761b6
L5vD5 <L5vD5@korea.ac.kr>
2017-03-24 02:04:59 +0900

    no message

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
 const express = require('express');
 
 var app = express();
+//post 통신을 위한 body parser!!
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : false}));
 //jade!!
 app.set('view engine', 'jade');
 app.set('views', './views');
@@ -13,10 +16,39 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 /*********
+Form으로 접속
+*********/
+app.get('/form', function(req,res){
+	res.render('form');
+});
+
+app.post('/form_receiver', function(req,res){
+	var title = req.body.title;
+	var description = req.body.description;
+	res.send(title+" : "+description);
+});
+/*********
 Template으로 접속
 *********/
 app.get('/template', function(req,res){
 	res.render('temp', {title : 'Wow nodejs',time : Date()});
+});
+/*********
+Topic으로 접속
+*********/
+app.get('/topic/:id', function(req,res){
+	var topics = [
+    'Javascript is....',
+    'Nodejs is...',
+    'Express is...'
+  ];
+  var output = `
+  <a href="/topic/0">JavaScript</a><br>
+  <a href="/topic/1">Nodejs</a><br>
+  <a href="/topic/2">Express</a><br><br>
+  ${topics[req.params.id]}
+  `
+	res.send(output + req.params.id);
 });
 /*********
 Home으로 접속
